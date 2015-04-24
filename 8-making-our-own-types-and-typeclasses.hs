bbb105ef974106a6a8be58c1b1b0ac686913678d
gugu4-9 <augustin@mrlacour.fr>
2015-04-24 23:36:06 +0200

    huitième chapitre fait

--- 8-making-our-own-types-and-typeclasses.hs
+++ 8-making-our-own-types-and-typeclasses.hs
@@ -1,3 +1,4 @@
+import Data.List
 {-
  - We are going to create some types for a deck of cards
  - The cards need to have an ordering, based on the standard ranking http://en.wikipedia.org/wiki/Standard_52-card_deck#Rank_and_color
@@ -12,13 +13,16 @@
  - Uncomment the following declarations to complete the implementation, and provide an implementation for instance Show Card
  -}
 
---data Suit = 
---data Digit = 
---data Card = 
+data Suit = Clubs | Diamonds | Hearts | Spades deriving (Eq, Show)
+data Digit = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving (Eq, Ord, Bounded, Show)
+data Card = Card {digit :: Digit, suit :: Suit} deriving (Eq)
+
+instance Show Card where
+	show (Card {digit = d, suit = s}) = "The " ++ show d ++ " of " ++ show s
 
 -- We should be able to provide a function which returns the higher ranked card:
 betterCard :: Card -> Card -> Card
-betterCard x y = undefined
+betterCard x y = if digit y < digit x then x else y
 
 -- Here is a new Typeclass, which represents some kind of playing hand in a game.
 -- It returns True for a "winning hand", depending on the rules for the type of class we are playing with
@@ -27,13 +31,22 @@ class Hand a where
 
 -- Implement Hand for Card, where play returns true if the list contains the Ace of Spades
 instance Hand Card where
-    play c = undefined
+    play c = (Card {digit = Ace, suit = Spades}) `elem` c
 
--- Create a new Coin type
---data Coin = 
+-- Create a new Coin type (monnaie)
+data Coin = Pile | Face deriving (Show, Eq)
 
 -- Implement Hand for Coin, where play returns true if there are ten heads in a row in the list
 instance Hand Coin where
-	play c =  undefined
+	play c = any (>= 10) (map length $ group c)
 
 -- Have a play with implementing Hand for some other types, for instance Int and Bool
+-- Black Jack breaking > 21
+instance Hand Integer where
+	play i = sum i <= 21
+
+-- Russian roulette
+data Gun = Shot | Clic deriving (Eq, Show)
+
+instance Hand Gun where
+	play c = any (== Shot) c
